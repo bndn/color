5bd6a66e997f1018dc23f7142009847c20fb01e8
amuh <amuh@itu.dk>
2016-04-08 15:45:25 +0200

    Added implementation of Color Squashed previous commits down.

---
+++ src/Color/Color.fs
@@ -0,0 +1,79 @@
+/// Copyright (C) 2016 The Authors.
+module Color
+
+type Color =
+    | C of float * float * float
+    override c.ToString() =
+        match c with
+        | C(r, g, b) -> "[R:" + r.ToString() + ", G:" + g.ToString() + ", B:" + b.ToString() + "]"
+/// <summary>
+/// Raised in case of attempting to input an invalid float value.
+/// </summary>
+exception InvalidFloatInputException
+
+/// <summary>
+/// Creates a color from three float values.
+/// </summary>
+/// <param name=r>The red colorspace floating value.</param>
+/// <param name=g>The green colorspace floating value.</param>
+/// <param name=b>The blue colorspace floating value.</param>
+/// <returns>The created Color.</returns>
+let make r g b =
+    if r > 1.0 || r < 0.0
+    then raise InvalidFloatInputException
+
+    if g > 1.0 || g < 0.0
+    then raise InvalidFloatInputException
+
+    if b > 1.0 || b < 0.0
+    then raise InvalidFloatInputException
+
+    C(r, g, b)
+
+/// <summary>
+/// Gets the red colorspace floating value from a Color.
+/// </summary>
+/// <param name=c>The color to retrieve the colorspace from.</param>
+/// <returns>The red colorspace floating value.</returns>
+let getR (C(r,_,_)) = r
+
+/// <summary>
+/// Gets the green colorspace floating value from a Color.
+/// </summary>
+/// <param name=c>The color to retrieve the colorspace from.</param>
+/// <returns>The green colorspace floating value.</returns>
+let getG (C(_,g,_)) = g
+
+/// <summary>
+/// Gets the blue colorspace floating value from a Color.
+/// </summary>
+/// <param name=c>The color to retrieve the colorspace from.</param>
+/// <returns>The blue colorspace floating value.</returns>
+let getB (C(_,_,b)) = b
+
+/// <summary>
+/// Scales a Color by a float value.
+/// </summary>
+/// <param name=c>The Color to scale.</param>
+/// <param name=s>The float value to scale by.</param>
+/// <returns>A new color scaled by s.</returns>
+let scale (C(r, g, b)) s =
+    if s < 0.0
+        then raise InvalidFloatInputException
+
+    C(min (r * s) 1.0, min (g * s) 1.0, min (b *s) 1.0)
+
+/// <summary>
+/// Merges two colors with respect to a reflective surface.
+/// </summary>
+/// <param name=refl>The float value denoting the reflective index.</param>
+/// <param name=c1>First color to mix with.</param>
+/// <param name=c2>Second color to mix with.</param>
+/// <returns>The result color of the mixing.</returns>
+let merge refl c1 c2 =
+    if refl > 1.0 || refl < 0.0
+    then raise InvalidFloatInputException
+
+    C(refl * getR(c1) + (1.0 - refl) * getR(c2),
+        refl * getG(c1) + (1.0 - refl) * getG(c2),
+        refl * getB(c1) + (1.0 - refl) * getB(c2))
---
+++ test/Color.Test/ColorTest.fs
@@ -0,0 +1,89 @@
+﻿/// Copyright (C) 2016 The Authors.
+module Color.Test
+
+open Xunit;
+open FsUnit.Xunit;
+
+[<Fact>]
+let ``make constructs a color from three float values between 0.0 and 1.0``() =
+    let c = Color.make 0.56 0.44 0.1
+
+    // Check that color was constructed
+    c |> should be instanceOfType<Color>
+
+[<Fact>]
+let ``getR gets the red colorspace of a color``() =
+    let c = Color.make 0.56 0.44 0.1
+
+    // Check red colorspace can be retrieved
+    Color.getR c |> should equal 0.56
+
+[<Fact>]
+let ``getG gets the green colorspace of a color``() =
+    let c = Color.make 0.56 0.44 0.1
+
+    // Check green colorspace can be retrieved
+    Color.getG c |> should equal 0.44
+
+[<Fact>]
+let ``getB gets the blue colorspace of a color``() =
+    let c = Color.make 0.56 0.44 0.1
+
+    // Check blue colorspace can be retrieved
+    Color.getB c |> should equal 0.1
+
+[<Fact>]
+let ``scale scales a color by a float value``() =
+    let c1 = Color.make 0.56 0.44 0.1
+    let c2 = Color.scale c1 0.33
+
+    // Check that the computed scaling is correct within +-0.01
+    abs (Color.getR c2 - 0.1848) |> should be (lessThan 0.01)
+    abs (Color.getG c2 - 0.1452) |> should be (lessThan 0.01)
+    abs (Color.getB c2 - 0.033) |> should be (lessThan 0.01)
+
+[<Fact>]
+let ``scale fails if float val is less than 0.0``() =
+    let c = Color.make 0.56 0.44 0.1
+
+    // Checks that scaling fails if float val is less than 0.0
+    (fun() -> Color.scale c -0.33 |> ignore) |> shouldFail
+
+[<Fact>]
+let ``scale does not allow scaling colors beyond 1.0``() =
+    let c1 = Color.make 0.56 0.44 0.1
+    let c2 = Color.scale c1 11.0
+
+    // Check that colorspaces does not scale beyond 1.0
+    Color.getR c2 |> should be (lessThanOrEqualTo 1.0)
+    Color.getG c2 |> should be (lessThanOrEqualTo 1.0)
+    Color.getB c2 |> should be (lessThanOrEqualTo 1.0)
+
+
+[<Fact>]
+let ``merge merges two colors with respect to a reflective surface index represented by a float``() =
+    let c1 = Color.make 0.56 0.44 0.1
+    let c2 = Color.make 0.20 0.54 0.2
+
+    let c3 = merge 0.5 c1 c2
+
+    // Check that the computed merging is correct within +-0.01
+    abs (Color.getR c3 - 0.38) |> should be (lessThan 0.01)
+    abs (Color.getG c3 - 0.49) |> should be (lessThan 0.01)
+    abs (Color.getB c3 - 0.15) |> should be (lessThan 0.01)
+
+[<Fact>]
+let ``merge fails if the reflective index is less than 0.0``() =
+    let c1 = Color.make 0.56 0.44 0.1
+    let c2 = Color.make 0.20 0.54 0.2
+
+    // Checks that merging fails if float val is less than 0.0
+    (fun() -> Color.merge -0.2 c1 c2 |> ignore) |> shouldFail
+
+[<Fact>]
+let ``merge fails if the reflective index is more than 1.0``() =
+    let c1 = Color.make 0.56 0.44 0.1
+    let c2 = Color.make 0.20 0.54 0.2
+
+    // Checks that merging fails if float val is more than 1.0
+    (fun() -> Color.merge 1.4 c1 c2 |> ignore) |> shouldFail
